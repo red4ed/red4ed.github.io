0f8387c9d46f397a77f59a32964503a071a7fb34
red4ed <36377617+red4ed@users.noreply.github.com>
2019-05-09 21:09:11 +0300

    edit code

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("878337221:AAEb7XdIlPMBgD9eauCTGbGfWOYAT77DsRY")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-	#bot.reply_to(message, message.text)
-	bot.send_message(message.chat.id, message.text)
+	bot.reply_to(message, message.text)
+	#bot.send_message(message.chat.id, message.text)
 
 bot.polling(none_stop=True)
